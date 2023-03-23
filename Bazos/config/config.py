7c686b80034041d6443293e52e9b43885fc0ba4d
Workaholic <128381647+ruWorkaholic-dev@users.noreply.github.com>
2023-03-23 20:09:28 +0200

    Update config.py

--- Bazos/config/config.py
+++ Bazos/config/config.py
@@ -1,5 +1,15 @@
 #Example -> BOT_API = "5862341293:AALKrq1234LJndGOAMe_glkjoj234H0r4"
 BOT_API = ""
 
-##Example -> PROXY = "http://8y2d223134x:NkMuLoO1w2s@45.128.62.16:8080"
+#Example -> PROXY = "http://8y2d223134x:NkMuLoO1w2s@45.128.62.16:8080"
 PROXY = ""
+
+#WhatsApp Short-link template:
+
+# [product_name] - Title product/Product name/Example -> Mobile phone
+# [product_location] - Location/Example -> Senec
+# [product_price] - Price/Example -> 300
+# [product_link] - Link on product/ Example -> https://dom.bazos.sk/inzerat/149179666/tvrde-drevo-na-kurenie.php
+
+#Example -> "Dobrý deň, obraciam sa na vás v súvislosti s [product_name]. Mám záujem o jej kúpu.  Sú na ňom nejaké známky používania, ktoré nie sú viditeľné na fotografii?"
+TEXT = ""
